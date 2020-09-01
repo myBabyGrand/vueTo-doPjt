9dfe7bf125945b1279f4ac67566386d2baba8e2c
mybabygrand <amuro_ray@kakao.com>
2020-09-01 20:18:47 +0900

    to-do app finish

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,72 @@
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="app">
+  <TodoHeader></TodoHeader>
+  <TodoInput v-on:addTodo="addTodo"></TodoInput>
+  <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+  <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
+</div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      todoItems: []
     }
+  },
+  methods: {
+    clearAll() {
+      localStorage.clear();
+      console.log("All cleared");
+      this.todoItems = [];
+    },
+		addTodo(todoItem) {
+			localStorage.setItem(todoItem, todoItem);
+			this.todoItems.push(todoItem);
+      console.log("todo added : "+todoItem);
+		},
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      console.log("removed : "+todoItem,index);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created() {
+		if (localStorage.length > 0) {
+			for (var i = 0; i < localStorage.length; i++) {
+				this.todoItems.push(localStorage.key(i));
+			}
+		}
+  },
+  components: {
+  'TodoHeader': TodoHeader,
+  'TodoInput': TodoInput,
+  'TodoList': TodoList,
+  'TodoFooter': TodoFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }
 </style>
